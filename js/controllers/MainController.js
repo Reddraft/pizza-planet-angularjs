e5daedd1f8281fb9b3ccd36c900cccc0f4cc538b
Ricardo <rick@reddraft.com>
2015-02-24 22:15:58 -0500

    project changes to index, controller and module

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,48 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
-    }
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
+    }    
+  ];
+
+    $scope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marianara Sauce, Mozzarella, fresh Basil',
+      price: 15.50
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Served with marinara sauce.',
+      price: 9.90
+    },
+    {
+      name: 'Rustica',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 12.00
+    }    
+  ];
+
+    $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Soup of the Day',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Buffalo Wings',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
+    }    
   ];
 
 }]);
